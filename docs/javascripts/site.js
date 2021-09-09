7d735720e6f6dc061148e819dcd0a7d4ca3c1142
Anton Tekutov <anton.tekutow@yandex.ru>
2021-09-09 18:06:32 +0300

    Auth deeplink test

--- docs/javascripts/site.js
+++ docs/javascripts/site.js
@@ -1,1 +1,12 @@
 // This is where it all goes :)
+
+function openApp() {
+    var urlString = window.location.href;
+    var url = new URL(urlString);
+    var code = url.searchParams.get("code");
+
+    var deeplinkUrlString = "whynft://auth?code=" + code;
+
+    var deeplinkUrl = new URL(deeplinkUrlString);
+    window.open(deeplinkUrl);
+}
